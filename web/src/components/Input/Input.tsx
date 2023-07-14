12e83189630d11e4061bdcbb7aad46f47167a276
XiaoPan <2489964425@qq.com>
2023-07-14 22:32:34 +0800

    feat(signIn.tsx):add signIn page style

--- web/src/components/Input/Input.tsx
+++ web/src/components/Input/Input.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import classNames from "classnames";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    label?: string;
-    className?: string;
+  label?: string;
+  className?: string;
 }
 
-export const Input: React.FC<InputProps> = ( props ) => {
-    const { className, label, ...rest } = props;
-    const cn = classNames("grow-[1] shrink-0 basis-1", className);
+export const Input: React.FC<InputProps> = (props) => {
+  const { className, label, children, ...rest } = props;
+  const cn = classNames("grow-[1] shrink-0 basis-1", className, {
+    "border-r-[red]": children,
+  });
 
-    return (
-        <div className="flex justify-center">
-            { label ? <div className="text-left text-nowrap flex items-center">{ label }</div> : null }
-            <input className={ cn } { ...rest } />
-        </div>
-    );
+  return (
+    <div className="flex justify-center">
+      {label ? (
+        <div className="text-left text-nowrap flex items-center">{label}</div>
+      ) : null}
+      <input className={cn} {...rest} />
+      {children}
+    </div>
+  );
 };
