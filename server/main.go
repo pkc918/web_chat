538bf7b812fe618713d4dc9e1d9acab7b709bc3f
PanKeChun <2489964425@qq.com>
2023-08-02 18:05:20 +0800

    chore(main): connect mysql

--- server/main.go
+++ server/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"database/sql"
+	"web_chat/server/db"
+	"web_chat/server/initialize"
 	"web_chat/server/router"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//var port string
-	//initialize.InitConfig()
-	//
-	//initPort := initialize.GetPort()
-	//initDb := db.InitDb()
-	//sqlDb, _ := initDb.DB()
-	//defer func(sqlDb *sql.DB) {
-	//	_ = sqlDb.Close()
-	//}(sqlDb)
+	var port string
+	initialize.InitConfig()
+
+	initPort := initialize.GetPort()
+	initDb := db.InitDb()
+	sqlDb, _ := initDb.DB()
+	defer func(sqlDb *sql.DB) {
+		_ = sqlDb.Close()
+	}(sqlDb)
 
 	r := gin.Default()
 	r = router.CollectRoute(r)
-	//if initPort != "" {
-	//	port = ":" + initPort
-	//}
-	panic(r.Run()) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if initPort != "" {
+		port = ":" + initPort
+	}
+	panic(r.Run(port)) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
